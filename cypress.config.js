3cab4106ea3ae963d69343935a3d9909b01363c2
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2022-09-22 15:55:51 +0200

    find elements

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  viewportHeight: 1080,
-  viewportWidth: 1020,
+  viewportHeight: 1800,
+  viewportWidth: 1240,
   e2e: {
-    baseUrl: 'http://localhost:42000',
+    baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*']
   }
--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,8 +1,26 @@
+
 describe('Our first suite', () => {
     beforeEach('', () => {
-
+        cy.visit('/')
     })
     it('First test', () => {
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+        cy.get('[data-cy="imputEmail1"]')
+    })
 
+    it('Second test', () => {
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+        cy.get('[data-cy="signInButton"]')
+        cy.contains('Sign in')
+        cy.contains('[status="warning"]', 'Sign in')
+        cy.get('#inputEmail3')
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click()
     })
 })
