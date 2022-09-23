f6907b0cd0da2154135978497e84b6c92c886cb2
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2022-09-23 08:40:42 +0200

    saving the subject of the command

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -23,4 +23,25 @@ describe('Our first suite', () => {
             .find('nb-checkbox')
             .click()
     })
+
+    it.only('then and wrap methods', () => {
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+        cy.contains('nb-card', 'Using the Grid').then(firstForm => {
+            const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
+            const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+            expect(emailLabelFirst).to.equal('Email')
+            expect(passwordLabelFirst).to.equal('Password')
+
+            cy.contains('nb-card', 'Basic form').then(secondForm => {
+                const emailLabelSecond = secondForm.find('[for="exampleInputEmail1"]').text()
+                const passwordLabelSecond = secondForm.find('[for="exampleInputPassword1"]').text()
+                expect(emailLabelSecond).to.equal('Email address')
+                expect(passwordLabelFirst).to.equal(passwordLabelSecond)
+
+                cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain', 'Password')
+            })
+
+        })
+    })
 })
