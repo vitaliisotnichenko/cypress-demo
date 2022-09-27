cfd765c5536ae46f6490d1e6bbc4a96167d7b16b
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2022-09-27 11:29:13 +0200

    checkboxes and radio buttons

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -71,4 +71,31 @@ describe('Our first suite', () => {
             cy.wrap(input).invoke('prop', 'value').should('contain', '2022')
         })
     })
+
+    it('radio button', () => {
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then(radioButtons => {
+            cy.wrap(radioButtons)
+                .first()
+                .check({force: true})
+                .should('be.checked')
+            cy.wrap(radioButtons)
+                .eq(1)
+                .check({force: true})
+                .should('be.checked')
+            cy.wrap(radioButtons)
+                .eq(0)
+                .should('not.be.checked')
+            cy.wrap(radioButtons)
+                .eq(2)
+                .should('be.disabled')
+        })
+    })
+
+    it('checkboxes', () => {
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+        cy.get('[type="checkbox"]').check({force: true})
+    })
 })
