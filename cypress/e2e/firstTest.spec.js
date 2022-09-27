d528b865a2d1757e96cfe3db55864afbc4239846
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2022-09-27 12:44:55 +0200

    lists and dropdowns

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -98,4 +98,31 @@ describe('Our first suite', () => {
         cy.contains('Toastr').click()
         cy.get('[type="checkbox"]').check({force: true})
     })
+
+    it.only('lists and dropdown', () => {
+        cy.get('nav nb-select').click()
+        cy.get('.options-list').contains('Dark').click()
+        cy.get('nav nb-select').should('contain', 'Dark')
+        cy.get('nb-layout-header nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')
+
+        cy.get('nav nb-select').then(dropdown => {
+            cy.wrap(dropdown).click()
+            cy.get('.options-list nb-option').each((listItem, index) => {
+                const itemText = listItem.text().trim()
+                const colors = {
+                    "Light": "rgb(255, 255, 255)",
+                    "Dark": "rgb(34, 43, 69)",
+                    "Cosmic": "rgb(50, 50, 89)",
+                    "Corporate": "rgb(255, 255, 255)"
+                }
+
+                cy.wrap(listItem).click()
+                cy.wrap(dropdown).should('contain', itemText)
+                cy.get('nb-layout-header nav').should('have.css', 'background-color', colors[itemText])
+                if(index < 3) {
+                    cy.wrap(dropdown).click()
+                }
+            })
+        })
+    })
 })
